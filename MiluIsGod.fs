9a71c1d7c17154ab9605bb612632d2f6afadc5f0
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-07-09 22:22:33 +0800

    添加 F# VB，删除 exe

---
+++ MiluIsGod.fs
@@ -0,0 +1,4 @@
+let mutable i = 0
+while true do
+    printfn "米露是神"
+    i <- i + 1
---
+++ MiluIsGod.vb
@@ -0,0 +1,9 @@
+Module Module1
+    Sub Main()
+        Dim i As Integer
+        For i = 1 To 10000
+            Console.WriteLine("米露是神")
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
