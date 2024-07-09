6fd9e182ae477907cbfb90020210f7c8657fe2a9
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-07-09 22:49:20 +0800

    add: Objective-C

---
+++ MiluIsGod.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        while (1) {
+            printf("米露是神\n");
+        }
+    }
+    return 0;
+}
